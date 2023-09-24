244f2786635713a9049ec3e9dedfa2510b64c4db
Ebrahim Mansour A Sharka <ebraandomar@gmail.com>
2023-09-24 20:32:18 +0300

    Update Page 1.py

--- Page 1.py
+++ Page 1.py
@@ -2,45 +2,40 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import nnfs  
+import nnfs
+from sklearn.utils import shuffle
 
-np.random.seed(20)
+# Set the random seed for reproducibility
+np.random.seed(42)
+nnfs.init()
+
 # Define the Layer class
 class Layer:
     def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
         self.output = None
 
     def forward(self, inputs):
-        # Calculate output values from inputs, weights, and biases
         self.output = np.dot(inputs, self.weights) + self.biases
 
     def ReLU(self):
-        # Apply ReLU without saving
         return np.maximum(0, self.output)
 
     def ApplyReLU(self):
-        # Apply ReLU and save the results in output
         self.output = np.maximum(0, self.output)
 
     def ApplySoftMax(self):
-        # Apply Softmax activation function
         exp_values = np.exp(self.output - np.max(self.output, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
 # Define the cross-entropy loss function
 def cross_entropy_loss(predictions, targets):
-    # Avoid numerical instability by adding a small epsilon value
     epsilon = 1e-15
     predictions = np.clip(predictions, epsilon, 1 - epsilon)
-    
-    # Calculate the cross-entropy loss
     N = predictions.shape[0]
     ce_loss = -np.sum(np.log(predictions[np.arange(N), targets])) / N
-    
     return ce_loss
 
 # Define the accuracy calculation function
@@ -57,78 +52,62 @@ def load_arabic_data():
     y = []
     X = []
 
-    for digit in range(2):
+    for digit in range(10):
         path = path_init + "//" + str(digit)
         dir_list = os.listdir(path)
-        print(path)
 
         for imageFile in dir_list:
             img = plt.imread(path + "//" + imageFile)
-            arr = np.array(img, dtype=float)  # Change this line
+            arr = np.array(img, dtype=float)
             flat_arr = arr.ravel()
             X.append(flat_arr)
             y.append(digit)
-            
 
-    return np.array(X, dtype=float), np.array(y, dtype=int)  # Change this line
+    return np.array(X, dtype=float), np.array(y, dtype=int)
 
 # Define your neural network architecture
 def build_neural_network():
-    nnfs.init()  # Initialize your neural network library
-    X, y = load_arabic_data()  # Load the Arabic numeral dataset
+    X, y = load_arabic_data()
+
+    # Shuffle the dataset
+    X, y = shuffle(X, y, random_state=42)
 
     # Create Dense layers with appropriate input and output sizes
-    layer1 = Layer(784, 784)  # Adjust the input size and output size
-    layer2 = Layer(784, 10)   # Adjust the input size and output size
-
-    # Initialize weights and biases for both layers once at the beginning
-    layer1.weights = 0.01 * np.random.randn(784, 784)
-    layer1.biases = np.zeros((1, 784))
-    layer2.weights = 0.01 * np.random.randn(784, 10)
-    layer2.biases = np.zeros((1, 10))
+    layer1 = Layer(784, 784)
+    layer2 = Layer(784, 10)
 
     # Define batch size and initial learning rate
     batch_size = 32
-    initial_learning_rate = 0.01
+    initial_learning_rate = 0.005
 
     # Training loop
-    lowest_loss = float('inf')  # Set an initial high loss value
+    lowest_loss = float('inf')
     best_weights1 = None
     best_biases1 = None
     best_weights2 = None
     best_biases2 = None
 
-    for iteration in range(10000):
-        # Shuffle the dataset and split it into mini-batches
-        indices = np.arange(len(X))
-        np.random.shuffle(indices)
-        X_shuffled = X[indices]
-        y_shuffled = y[indices]
+    for iteration in range(2000):
+        for i in range(0, len(X), batch_size):
+            X_batch = X[i:i+batch_size]
+            y_batch = y[i:i+batch_size]
 
-        for i in range(0, len(X_shuffled), batch_size):
-            # Get mini-batch
-            X_batch = X_shuffled[i:i+batch_size]
-            y_batch = y_shuffled[i:i+batch_size]
+            # Set the random seed before updating weights
+            np.random.seed(iteration + i)
 
-            # Update weights with small random values
             layer1.weights += initial_learning_rate * np.random.randn(784, 784)
             layer1.biases += initial_learning_rate * np.random.randn(1, 784)
             layer2.weights += initial_learning_rate * np.random.randn(784, 10)
             layer2.biases += initial_learning_rate * np.random.randn(1, 10)
 
-            # Forward pass
             layer1.forward(X_batch)
             layer1.ApplyReLU()
             layer2.forward(layer1.output)
             layer2.ApplySoftMax()
 
-            # Calculate loss
             loss = cross_entropy_loss(layer2.output, y_batch)
-
-            # Calculate accuracy
             accuracy = calculate_accuracy(layer2.output, y_batch)
 
-            # Check if the current model has the lowest loss
             if loss < lowest_loss:
                 print('New set of weights found, iteration:', iteration,
                       'loss:', loss, 'acc:', accuracy)
@@ -138,20 +117,16 @@ def build_neural_network():
                 best_biases2 = layer2.biases.copy()
                 lowest_loss = loss
             else:
-                # Revert to the best weights and biases found so far
                 layer1.weights = best_weights1.copy()
                 layer1.biases = best_biases1.copy()
                 layer2.weights = best_weights2.copy()
                 layer2.biases = best_biases2.copy()
 
-        # Learning rate scheduling (reduce learning rate over time)
         if iteration % 100 == 0:
             initial_learning_rate *= 0.9
 
 # Main function
 def main():
-
-    # Train your neural network
     build_neural_network()
 
 if __name__ == "__main__":
